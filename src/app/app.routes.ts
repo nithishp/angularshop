fab5ef3e27a1c255645d09d803c22e409295ed37
nithishp <nithishj03@gmail.com>
2024-04-14 14:31:10 +0530

    Add HomeComponent and update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContentComponent } from '../content/content.component';
 import { AddmembersComponent } from '../addmembers/addmembers.component';
+import { HomeComponent } from '../home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent }, 
     { path: 'view', component: ContentComponent },
   { path: 'add', component: AddmembersComponent },
-  { path: '', component: ContentComponent }, 
 ];
